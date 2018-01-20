553a799f688870f49cb8925a0728921553bd01f1
Saikrishna Rao <krish98.sai@gmail.com>
2018-01-20 14:12:45 -0800

    allows unique registration with phone number

--- data-share-backend/app/controllers/application_controller.rb
+++ data-share-backend/app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def root
     render plain: "ROOT"
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone])
+  end
+
 end
--- data-share-backend/app/models/user.rb
+++ data-share-backend/app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable,
           :confirmable
+  validates :phone, presence: true, uniqueness: true
   include DeviseTokenAuth::Concerns::User
+
 end
