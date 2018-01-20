ba3381c780d92c32b3d5c6281028769920307692
Saikrishna Rao <krish98.sai@gmail.com>
2018-01-20 15:26:21 -0800

    adds check_token to check user authentication through the headers

--- data-share-backend/app/controllers/sessions_controller.rb
+++ data-share-backend/app/controllers/sessions_controller.rb
@@ -1,3 +1,8 @@
 class SessionsController < DeviseTokenAuth::SessionsController
-
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  def check_token
+    unless authenticate_user!
+      render json: "{\"status\": \"success\"}"
+    end
+  end
 end
--- data-share-backend/config/routes.rb
+++ data-share-backend/config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     sessions: 'sessions'
   }
+
+  devise_scope :user do
+    get 'check_token' => 'sessions#check_token'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
