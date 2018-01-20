a547f8f0e4c68c1aaffef9edca16fbbf60cb702f
Saikrishna Rao <krish98.sai@gmail.com>
2018-01-20 04:00:18 -0800

    Updates config initializer email for prod

--- data-share-backend/config/environments/production.rb
+++ data-share-backend/config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # default url
-  config.action_mailer.default_url_options = { :host => 'http://www.get-data-share.com' }
+  config.action_mailer.default_url_options = { :host => 'http://get-data-share.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
